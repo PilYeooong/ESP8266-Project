07762c485f1fc00fd1694f837c7c16df0a490c69
PilYeooong <pilyeooong@gmail.com>
2019-06-23 22:21:29 +0900

    fixed

--- Autowindow/Autowindow.ino
+++ Autowindow/Autowindow.ino
@@ -1,152 +1,29 @@
-#include <Stepper.h>
-const int TMP36_pin = A1; //온도
 
-const int stepsPerRevolution = 200; //회전수  200/한바퀴
-Stepper myStepper(stepsPerRevolution, 3, 4, 6, 7);
-int ENA = 8;
-int ENB = 5;
-int enablePin = 11;
-int in1Pin = 10;
-int in2Pin = 9;
+#include <Servo.h>
+#include <DHT11.h>
 
-int ondo1 = 32;
-int ondo2 = 30;
-int gas1 = 400;
-int gas2 = 200;
+DHT11 dht11(D3);
+Servo servo;
+
+//서보모터 - 주황 D4 , 빨강 3v3 , 갈색 gnd
+int ondo = 20; // 온도
+int ondo2 = 15; //최저온도
+int ondo3 = 29;
+int dust = 400; //먼지
 
 boolean window = false;   //false -> 닫혀있는상태
-              //true -> 열려있는상태
+                          //true -> 열려있는상태
 
-const int gasPin = A0; // 가스
+const int dustPin = A0; // 먼지
 
-void setup()
-{
-  myStepper.setSpeed(60);
-  Serial.begin(9600);
-  pinMode(enablePin, OUTPUT);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
-  pinMode(ENA, OUTPUT); //
-  pinMode(ENB, OUTPUT);//
-  pinMode(2, INPUT);
-  digitalWrite(ENA, HIGH);//
-  digitalWrite(ENB, HIGH);//
-}
 
-void loop()
-{
-  int sensorValue = analogRead(TMP36_pin);
-  float mVoltage = sensorValue * 5000.0 / 1024.0;
-  float temperatureC = (mVoltage - 500) / 10.0;
-  int value = digitalRead(2);
-
-  Serial.print("Now : ");
-  Serial.print(temperatureC);
-  Serial.println(" C");
-  Serial.println();
-  Serial.print("current gas : ");
-  Serial.println(analogRead(gasPin));
-  Serial.println();
-  Serial.print("Rain : ");
-  Serial.println(value);
-  Serial.print("Window : ");
-  if (window == false) Serial.println("Close");
-  else if (window == true) Serial.println("Open");
-  Serial.println("=============================================== ");
-  Serial.println();
-
-  if (window == false && value == 1) //창문이 닫혀있고 비가 안오는 상태
-  {
-    if (temperatureC <= ondo2 && analogRead(gasPin) >= gas1)
-    {
-      sensor_open();
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) >= gas1)
-    {
-      sensor_open();
-    }
-    else if (temperatureC <= ondo2 && analogRead(gasPin) <= gas2)
-    {
-      // 동작안함
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) <= gas2)
-    {
-      sensor_open();
-    }
-  }
-
-  else if (window == false && value == 0) //창문이 닫혀있고 비가 오는 상태
-  {
-    if (temperatureC <= ondo2 && analogRead(gasPin) >= gas1)
-    {
-      sensor_open();
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) >= gas1)
-    {
-      sensor_open();
-    }
-    else if (temperatureC <= ondo2 && analogRead(gasPin) <= gas2)
-    {
-      // 동작안함
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) <= gas2)
-    {
-      //동작안함
-    }
-    else
-    {
-      //동작안함
-    }
-  }
-
-  else if (window == true && value == 1) //창문이열려있고 비가 안오는 상태 
-  {
-    if (temperatureC <= ondo2 && analogRead(gasPin) >= gas1)
-    {
-      //동작안함
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) >= gas1)
-    {
-      //동작안함
-    }
-    else if (temperatureC <= ondo2 && analogRead(gasPin) <= gas2)
-    {
-      sensor_close();
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) <= gas2)
-    {
-      //동작안함
-    }
-  }
-
-  else if (window == true && value == 0) //창문이열려있고 비가 오는 상태
-  {
-    if (temperatureC <= ondo2 && analogRead(gasPin) >= gas1)
-    {
-      //동작안함
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) >= gas1)
-    {
-      //동작안함
-    }
-    else if (temperatureC <= ondo2 && analogRead(gasPin) <= gas2)
-    {
-      sensor_close();
-    }
-    else if (temperatureC >= ondo1 && analogRead(gasPin) <= gas2)
-    {
-      sensor_close();
-    }
-    else
-    {
-      sensor_close();
-    }
-  }
-  delay(5000);
-}
 void sensor_open() {
   Serial.println("open");
-  myStepper.step(stepsPerRevolution);
+  for(int angle = 0; angle < 180; angle++) 
+  { 
+    servo.write(angle); 
+    delay(15); 
+  } 
   Serial.println("stop");
   window = true;
 
@@ -159,10 +36,95 @@ void sensor_open() {
 void sensor_close()
 {
   Serial.println("Close");
-  myStepper.step(-stepsPerRevolution);
+  for(int angle = 180; angle > 0; angle--) 
+  { 
+    servo.write(angle); 
+    delay(15); 
   window = false;
 
   Serial.print("Window Status : ");
   Serial.println(window);
   Serial.println();
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(2); //D2
+  servo.write(0);
+}
+
+void loop()
+{
+  
+  float temp,humi;
+  int result = dht11.read(humi,temp);
+
+  Serial.print("Temperature : ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println();
+  // Serial.print("current dust : ");
+  // Serial.println(analogRead(dustPin));
+  Serial.println();
+  Serial.print("Humidity : ");
+  Serial.println(humi);
+  
+  Serial.print("Window : ");
+  if (window == false) Serial.println("Close");
+  else if (window == true) Serial.println("Open");
+  
+  Serial.println("=============================================== ");
+  Serial.println();
+
+  if (window == false && humi < 50) //창문이 닫혀있고 비 안옴  
+  {
+    if (temp >= ondo)
+    {
+      sensor_open();
+    }
+    else if (temp <= ondo)
+    {
+      // 동작안함
+    }
+  }
+  else if(window == false && humi > 50) 
+  {
+    if (temp <= ondo)
+    {
+      // 동작안함
+    }
+    else if (temp >= ondo3)
+    {
+      sensor_open();
+    }
+    else if (analogRead(dustPin) >= dust) // 집안 공기가 좋지않을 때
+    {
+      sensor_open();
+    }
+  }
+
+  else if (window == true && humi < 35) //창문이열려있고 비가 안오는 상태 
+  {
+    if (temp < ondo2)
+    {
+      sensor_close();
+    }
+    else if (temp >= ondo || analogRead(dustPin) >= dust)
+    {
+      //동작안함
+    }
+  }
+
+  else if (window == true && humi > 50) //창문이열려있고 비가 오는 상태
+  {
+   if (temp > ondo3 || analogRead(dustPin) >= dust){
+      // 동작안함
+    }
+    else{
+      sensor_close();
+    }
+  }
+  delay(5000);
 }
