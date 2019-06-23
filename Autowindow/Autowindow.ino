c0043eb679cf680625fadef19c7faba6603dfc56
PilYeooong <pilyeooong@gmail.com>
2019-06-23 22:58:09 +0900

    fixed

--- Autowindow/Autowindow.ino
+++ Autowindow/Autowindow.ino
@@ -1,4 +1,3 @@
-
 #include <Servo.h>
 #include <DHT11.h>
 
@@ -6,16 +5,89 @@ DHT11 dht11(D3);
 Servo servo;
 
 //서보모터 - 주황 D4 , 빨강 3v3 , 갈색 gnd
-int ondo = 20; // 온도
+
+int ondo = 25; // 온도
 int ondo2 = 15; //최저온도
 int ondo3 = 29;
-int dust = 400; //먼지
+
+int dustPin = A0; // 먼지
+int dustLED = 2;
+float dust_value = 0;
+float Voltage = 0;
+float Dust = 0;
 
 boolean window = false;   //false -> 닫혀있는상태
                           //true -> 열려있는상태
 
-const int dustPin = A0; // 먼지
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(2); //D2
+  servo.write(0);
+  pinMode(dustLED,OUTPUT);
+  pinMode(dustPin,INPUT);
+}
 
+void loop()
+{
+  
+  float temp,humi;
+  int result = dht11.read(humi,temp);
+  Voltage = dust_value *5.0 /1024.0; 
+  Dust = (Voltage - 0.3)/0.005; //사용 전압을 기준으로 미세먼지 값을 변환
+  
+  Serial.print("Temperature : ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println();
+  Serial.print("Dust : ");
+  Serial.println(Dust);
+  Serial.println();
+  Serial.print("Humidity : ");
+  Serial.println(humi);
+  
+  Serial.print("Window : ");
+  if (window == false) Serial.println("Close");
+  else if (window == true) Serial.println("Open");
+  
+  Serial.println("=============================================== ");
+  Serial.println();
+
+  if (window == false && humi < 45) //창문이 닫혀있고 비 안옴  
+  {
+    if (temp >= ondo && Dust <55 ) 
+    {
+      sensor_open();
+    }
+  }
+  
+  else if(window == false && humi > 60) // 창문이 닫혀있고 비가 올때
+  {
+ 
+    if (temp >= ondo3) //29도 이상으로 올라가면 창문 개방
+    {
+      sensor_open();
+    }
+  }
+
+  else if (window == true && humi < 45) //창문이열려있고 비가 안오는 상태 
+  {
+    if (temp < ondo2 || Dust > 60) // 15도 이하로 떨어지면 창문 닫음
+    {
+      sensor_close();
+    }
+    else if (temp >= ondo )
+    {
+      //동작안함
+    }
+  }
+
+  else if (window == true && humi > 60) //창문이열려있고 비가 오는 상태
+  {
+      sensor_close(); 
+  }
+  delay(5000);
+}
 
 void sensor_open() {
   Serial.println("open");
@@ -46,85 +118,4 @@ void sensor_close()
   Serial.println(window);
   Serial.println();
   }
-}
-
-void setup()
-{
-  Serial.begin(9600);
-  servo.attach(2); //D2
-  servo.write(0);
-}
-
-void loop()
-{
-  
-  float temp,humi;
-  int result = dht11.read(humi,temp);
-
-  Serial.print("Temperature : ");
-  Serial.print(temp);
-  Serial.println(" C");
-  Serial.println();
-  // Serial.print("current dust : ");
-  // Serial.println(analogRead(dustPin));
-  Serial.println();
-  Serial.print("Humidity : ");
-  Serial.println(humi);
-  
-  Serial.print("Window : ");
-  if (window == false) Serial.println("Close");
-  else if (window == true) Serial.println("Open");
-  
-  Serial.println("=============================================== ");
-  Serial.println();
-
-  if (window == false && humi < 50) //창문이 닫혀있고 비 안옴  
-  {
-    if (temp >= ondo)
-    {
-      sensor_open();
-    }
-    else if (temp <= ondo)
-    {
-      // 동작안함
-    }
-  }
-  else if(window == false && humi > 50) 
-  {
-    if (temp <= ondo)
-    {
-      // 동작안함
-    }
-    else if (temp >= ondo3)
-    {
-      sensor_open();
-    }
-    else if (analogRead(dustPin) >= dust) // 집안 공기가 좋지않을 때
-    {
-      sensor_open();
-    }
-  }
-
-  else if (window == true && humi < 35) //창문이열려있고 비가 안오는 상태 
-  {
-    if (temp < ondo2)
-    {
-      sensor_close();
-    }
-    else if (temp >= ondo || analogRead(dustPin) >= dust)
-    {
-      //동작안함
-    }
-  }
-
-  else if (window == true && humi > 50) //창문이열려있고 비가 오는 상태
-  {
-   if (temp > ondo3 || analogRead(dustPin) >= dust){
-      // 동작안함
-    }
-    else{
-      sensor_close();
-    }
-  }
-  delay(5000);
 }
